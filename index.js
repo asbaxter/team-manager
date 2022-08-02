e5e19c676dfeff00d6101c7f0ceefb6aba2f6e0c
Andrew Baxter <asbaxter1@gmail.com>
2022-08-02 18:46:57 -0400

    now apllication prompts for job title and asks for information based off title chosen

--- index.js
+++ index.js
@@ -1,19 +1,82 @@
 const inquirer = require("inquirer");
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
 
+function promptUser(){
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employees name: ",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the employees ID: ",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the employees email:",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "Provide employees title:",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "title",
+      }
+    ])
+    .then(function ({ name, id, email, title }){
+        
+        if (title == "Manager"){
+            inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your manager's office number: ",
+                    name: "titleAttribute"
+                }
+            ])
+            .then(function ({titleAttribute}){
+                console.log(name)
+                console.log(id)
+                console.log(email)
+                console.log(titleAttribute)
+            })
+        } else if(title == "Engineer"){
+            inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your engineer's GitHub",
+                    name: "titleAttribute"
+                }
+            ])
+            .then(function ({titleAttribute}){
+                console.log(name)
+                console.log(id)
+                console.log(email)
+                console.log(titleAttribute)
+            })
+        } else{
+            inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your interns school: ",
+                    name: "titleAttribute"
+                }
+            ])
+            .then(function ({titleAttribute}){
+                console.log(name)
+                console.log(id)
+                console.log(email)
+                console.log(titleAttribute)
+            })
 
-function init(){
-    
-    new Employee().initialPrompt();
+        }
+    })
 
-    //new Manager().promptOffice();
-
-    //new Engineer().promptGitHub();
-
-    //new Intern().promptSchool();
+   
 }
 
-init();
+promptUser();
