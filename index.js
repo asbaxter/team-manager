28d42c2f0bb39d1e5be12fbe20edacacd445c821
Andrew Baxter <asbaxter1@gmail.com>
2022-08-03 17:41:27 -0400

    application now generate stylesheet with basic css

--- index.js
+++ index.js
@@ -109,15 +109,17 @@ function createHTML(){
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
         <title>Team Manager</title>
     </head>
     <body>
         <header>
             <h1>Team Manager</h1>
         </header>
+
+        <div class="row justify-content-center" id="team">
         
-    </body>
-    </html>
     `
 
     fs.writeFile("./dist/index.html", html, function (err) {
@@ -134,29 +136,35 @@ function createEmployeeCards(){
         let attributeName;
 
         if (employees[i].office != null){
-            console.log("created Manager")
+            jobTitle = 'Manager';
             jobAttribute = employees[i].office;
             attributeName = 'Office #:';
         }
         else if(employees[i].github != null){
-            console.log("created engineer")
+            jobTitle = 'Engineer';
             jobAttribute = employees[i].github;
             attributeName = 'GitHub: ';
         } else {
-            console.log("created intern")
             jobAttribute = employees[i].school;
+            jobTitle = 'Intern';
             attributeName = 'School: ';
         }
 
         let employeeHTML = `
-            <div>
+        <div class="col-sm-12 col-md-3"id="employeeCard">
+            <div id="employeeHeader">
                 <h2>${employees[i].name}</h2>
+                <h3>${jobTitle}</h3>
+            </div>
+            <div id="employeeInfo">
                 <ul>
                     <li>ID: ${employees[i].id}</li>
                     <li>Email: ${employees[i].email}</li>
                     <li>${attributeName + jobAttribute}</li>
                 </ul>
             </div>
+        </div>
+
         `
         fs.appendFile("./dist/index.html", employeeHTML, function (err) {
             if (err) {
@@ -165,5 +173,72 @@ function createEmployeeCards(){
             return resolve();
           });
     };
+    const htmlEnd = `
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    `
+    fs.appendFile("./dist/index.html", htmlEnd, function (err) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+      footerHTML();
+};
+function footerHTML(){
+    const htmlEnd = `
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    `
+    fs.appendFile("./dist/index.html", htmlEnd, function (err) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+
+      generateCSS();
+}
+
+
+function generateCSS(){
+
+    const css = `
+    header {
+        text-align: center;
+    }
+    li {
+        list-style: none;
+    }
+    #employeeCard{
+        border: black solid 2px;
+        margin: 20px;
+    }
+    #employeeHeader{
+        text-align: center;
+    }
+    #employeeInfo{
+        padding: 5px;
+    }
+    #employeeInfo ul li{
+        border: grey solid .5px; 
+        padding: 5px;
+    }
+    
+    `
+
+    fs.writeFile("./dist/style.css", css, function (err) {
+        if (err) {
+          console.log(err);
+        }
+    })
 };
 promptUser();
